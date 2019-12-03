280b3dfb7d73e3cef4e3a0b19296bf731adf92df
BethanyEJohnson <46764533+BethanyEJohnson@users.noreply.github.com>
2019-12-02 19:54:15 -0500

    Add files via upload
    Everything including a sample csv file

---
+++ src/application/CaptureAnalyzer.java
@@ -0,0 +1,30 @@
+package application;
+
+public class CaptureAnalyzer {
+	private int[] number;
+	 private double[] time;
+	 private String[] source;
+	 private String[] destination;
+	 private String[] protocol;
+	 private int[] length;
+	 private String[] info;
+	//This class provides methods for analyzing the parsed csv file
+	//An array list of packet objects and/or arraylists of each attribute may be useful
+	
+	
+	//display interface type
+	//display capture filter
+	//display link type
+	//display dropped packets percentage
+	//display # of packets
+	//display time span
+	//display average pps
+	//display pps in graph
+	//display average packet size
+	//get percentages of each protcol type
+
+	
+	
+	
+	
+}
---
+++ src/application/CsvParser.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvParser {
+	ArrayList<Packet> packets;
+	
+	public ArrayList<Packet> readCsv(String filePath) {
+		BufferedReader reader = null;
+
+		try {
+			List<Packet> packets = new ArrayList<Packet>();
+			String line = "";
+			reader = new BufferedReader(new FileReader(filePath));
+			reader.readLine();
+
+			while ((line = reader.readLine()) != null) {
+				String[] fields = line.split(",");
+
+				if (fields.length > 0) {
+					Packet packet = new Packet();
+					packet.setNumber(Integer.parseInt(fields[0].replace("\"", "")));
+					packet.setTime(Double.parseDouble(fields[1].replace("\"", "")));
+					packet.setSource(fields[2].replace("\"", ""));
+					packet.setDestination(fields[3].replace("\"", ""));
+					packet.setProtocol(fields[4].replace("\"", ""));
+					packet.setLength(Integer.parseInt(fields[5].replace("\"", "")));
+					packet.setInfo(fields[6].replace("\"", ""));
+					packets.add(packet);
+				}
+			}
+			// prints out read csv data to the console, this should be removed later
+			for (Packet p : packets) {
+				System.out.println(p.getNumber() + " " + p.getTime() + " " + p.getSource() + " " + p.getDestination()
+						+ " " + p.getProtocol() + " " + p.getLength() + " " + p.getInfo());
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				reader.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return packets;
+	}
+}
